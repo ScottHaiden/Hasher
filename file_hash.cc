e9ae5ad07c7353d50b798ef34eb461cf447a6bbe
Scott Haiden <scott.b.haiden@gmail.com>
2021-04-21 01:00:16 -0700

    Undo using madvise, apparently it does not help

--- file_hash.cc
+++ file_hash.cc
@@ -140,8 +140,6 @@ Mmap(int fd) {
 
     auto ptr = MakeUnique(static_cast<char*>(mapped), std::move(cleanup));
 
-    if (madvise(ptr.get(), file_len, MADV_WILLNEED)) DIE("madvise");
-
     return {std::move(ptr), file_len};
 }
 
